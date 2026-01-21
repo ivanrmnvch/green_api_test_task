49dcfb4cccf83610ab1a4a028d15ffc053786f15
ivanrmnvch <ivansylkin@gmail.com>
2026-01-21 19:15:44 +0300

    feat add deploy command

---
+++ vite.config.ts
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  base: '/',
+  // Для GitHub Pages, если репозиторий не в корне (username.github.io/repo-name):
+  // base: '/repo-name/',
+})
+
